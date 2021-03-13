40a154c300eda7ca4bcd74458cf5bb477c2bf4e1
Patrik Romanský <66646110@cuni.cz>
2021-03-13 19:51:31 +0100

    Enum Direction Adds comments

--- app/src/main/java/com/example/pacmanlike/objects/Direction.java
+++ app/src/main/java/com/example/pacmanlike/objects/Direction.java
@@ -1,17 +1,31 @@
 package com.example.pacmanlike.objects;
 
+/**
+ * Enum representing the different direction of moving objects,
+ * that are placed on the game map.
+ */
 public enum Direction {
+
     UP(0),
     DOWN(1),
     LEFT(2),
     RIGHT(3),
     NONE(4);
 
+    // iteration value
     private int value;
+
+    // Index
     Direction(int value) { this.value = value; }
 
+    // Direction To integer
     public int getValue() { return value; }
 
+    /**
+     * Converts the direction to rotation vector.
+     * @param direction Given direction.
+     * @return Rotation vector.
+     */
     public static Vector convertDirectionToVector(Direction direction) {
 
         switch (direction) {
@@ -28,6 +42,11 @@ public enum Direction {
         }
     }
 
+    /**
+     * Returns the opposite direction.
+     * @param direction Current direction.
+     * @return Opposite direction.
+     */
     public static Direction getOppositeDirection(Direction direction){
         switch (direction) {
             case UP:
@@ -41,6 +60,5 @@ public enum Direction {
             default:
                 return Direction.NONE;
         }
-
     }
 }
