462c6c1163335b1a657b1a4879741410bf85004b
Patrik Romanský <66646110@cuni.cz>
2021-03-13 21:56:12 +0100

    Class BFS Adds comments

--- app/src/main/java/com/example/pacmanlike/gamemap/BFS.java
+++ app/src/main/java/com/example/pacmanlike/gamemap/BFS.java
@@ -41,14 +41,24 @@ public class BFS {
         }
     }
 
-
+    // game map
     GameMap gameMap;
 
+    /**
+     * Constructor for the BFS search algorithm.
+     * The map parameter represents the graph being searched.
+     * @param map Gqme map
+     */
     public BFS(GameMap map){
         this.gameMap = map;
     }
 
-
+    /**
+     * Return true if exists path between start and target.
+     * @param start Start position.
+     * @param target Target position.
+     * @return true if there is a path.
+     */
     public boolean existsPath(Vector start, Vector target){
         List<Direction> tmp = findPath(start, target);
 
@@ -60,6 +70,12 @@ public class BFS {
     }
 
 
+    /**
+     * Returns true if the target is achievable.
+     * @param target Target position.
+     * @param node Start position.
+     * @return true if the target is achievable.
+     */
     public boolean achievedTarget(Vector target, Node node){
 
         if(target.x == node.position.x && target.y == node.position.y){
@@ -69,9 +85,16 @@ public class BFS {
         return false;
     }
 
+    /**
+     * Returns the sequence of steps from position to goal.
+     * Serves for the reconstruction of the road in BFS.
+     * @param node Last node.
+     * @return List of direction.
+     */
     public List<Direction> getPath(Node node) {
         List<Direction> tmp = new ArrayList<Direction>();
 
+        // coming back
         while (node.parent != null){
 
             tmp.add(node.direction);
@@ -82,6 +105,12 @@ public class BFS {
     }
 
 
+    /**
+     * BFS algorithm.
+     * @param start Start position.
+     * @param target Target Position.
+     * @return Path from start to target.
+     */
     public List<Direction> findPath(Vector start, Vector target){
 
         List<Direction> path = new ArrayList<Direction>();
@@ -99,27 +128,39 @@ public class BFS {
         // Create a queue for BFS
         LinkedList<Node> queue = new LinkedList();
 
-
+        // sets start vertex as visited
         visited[start.y][start.x] = true;
+
+        // add to queue
         queue.add(new Node(start, null, null));
 
         Node s;
+
+        // BFS
         while (queue.size() != 0) {
 
+            // pop vertex
             s = queue.poll();
             int x = s.position.x;
             int y = s.position.y;
 
+            // gets children
             List<Direction> possibleMoves = gameMap.getTile(x, y).getPossibleMoves();
             for (Direction d: possibleMoves) {
 
+                // according to the directions where you can get, choose the shelf following this
                 switch (d){
                     case LEFT:
                         if(!visited[y][x - 1]){
+
+                            // set visited
                             visited[y][x - 1] = true;
+
+                            // add to queue
                             Node node = new Node(new Vector(x - 1, y), s, Direction.LEFT);
                             queue.add(node);
 
+                            // target test
                             if(achievedTarget(target, node)){
                                 path = getPath(node);
                             }
@@ -127,10 +168,15 @@ public class BFS {
                         break;
                     case RIGHT:
                         if(!visited[y][x + 1]){
+
+                            // set visited
                             visited[y][x + 1] = true;
+
+                            // add to queue
                             Node node = new Node(new Vector(x + 1, y), s, Direction.RIGHT);
                             queue.add(node);
 
+                            // target test
                             if(achievedTarget(target, node)){
                                 path = getPath(node);
                             }
@@ -138,10 +184,15 @@ public class BFS {
                         break;
                     case DOWN:
                         if(!visited[y + 1][x]){
+
+                            // set visited
                             visited[y + 1][x] = true;
+
+                            // add to queue
                             Node node = new Node(new Vector(x, y + 1), s, Direction.DOWN);
                             queue.add(node);
 
+                            // target test
                             if(achievedTarget(target, node)){
                                 path = getPath(node);
                             }
@@ -150,10 +201,15 @@ public class BFS {
                         break;
                     case UP:
                         if(!visited[y - 1][x]){
+
+                            // set visited
                             visited[y - 1][x] = true;
+
+                            // add to queue
                             Node node = new Node(new Vector(x, y - 1), s, Direction.UP);
                             queue.add(node);
 
+                            // target test
                             if(achievedTarget(target, node)){
                                 path = getPath(node);
                             }
